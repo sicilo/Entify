c35a627a20d82e1b4f2bca1e5a65ec2c581b4e6f
Camilo Mejia Monsalve <mejiamonsalve.camilo@gmail.com>
2024-03-17 14:32:21 -0500

    rm: removing test and measuring

--- Entify.Tests/Models/Areas.cs
+++
@@ -1,8 +0,0 @@
-namespace Entify.Tests;
-
-public record Areas
-{
-    public int Id { get; init; }
-    public string Codigo { get; init; } = string.Empty;
-    public string Nombre { get; init; } = string.Empty;
-}
